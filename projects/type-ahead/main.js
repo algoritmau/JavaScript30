be0999dd3ba35c213760f0630f1899130e1520fa
Mauricio Paternina <spaceinvadev@gmail.com>
2021-02-22 13:40:40 -0500

    add type-ahead project

---
+++ projects/type-ahead/main.js
@@ -0,0 +1,62 @@
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+let cities = [];
+const searchInput = document.querySelector('.search-form__input');
+const searchResultsList = document.querySelector('.search-results-list');
+
+const getCities = async () => {
+  const payload = await fetch(endpoint);
+  cities = (await payload.json()).flat();
+};
+
+const findMatches = (wordToMatch, cities) => {
+  return cities.filter((place) => {
+    // Determine whether searched word matches city or state
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+};
+
+const formatNumberWithCommas = (x) =>
+  x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+const renderMatches = (e) => {
+  const resultsArray = findMatches(e.currentTarget.value, cities);
+  const resultsHtml = resultsArray
+    .map((result) => {
+      const searchedWord = new RegExp(e.currentTarget.value, 'gi');
+      const highlightedCity = result.city.replace(
+        searchedWord,
+        `<span class="highlighted">${e.currentTarget.value.toLowerCase()}</span>`
+      );
+      const highlightedState = result.state.replace(
+        searchedWord,
+        `<span class="highlighted">${e.currentTarget.value.toLowerCase()}</span>`
+      );
+
+      return `
+      <li class="search-results-list__item">
+        <span class="name">${highlightedCity}, ${highlightedState}</span>
+        <span class="population">${formatNumberWithCommas(
+          result.population
+        )}</span>
+      </li>
+    `;
+    })
+    .join('');
+
+  searchResultsList.innerHTML = resultsHtml;
+};
+
+searchInput.addEventListener('change', renderMatches);
+searchInput.addEventListener('keyup', renderMatches);
+
+// city: "Los Angeles"
+// growth_from_2000_to_2013: "4.8%"
+// latitude: 34.0522342
+// longitude: -118.2436849
+// population: "3884307"​
+// rank: "2"
+// state: "California"
+
+getCities();
